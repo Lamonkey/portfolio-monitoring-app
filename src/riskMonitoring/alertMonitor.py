cff2025dc224ec5f2c07e1ac31cd0f28ce81101b
lamonkey <88therisingsun@gmail.com>
2023-11-02 10:34:48 -0400

    #30 StockPriceDailyPct,  StockPriceCumChange

--- src/riskMonitoring/alertMonitor.py
+++ src/riskMonitoring/alertMonitor.py
@@ -1,3 +1,4 @@
+from riskMonitoring import db_operation as db
 class PNLDown():
     def watch(self):
         pass
@@ -20,3 +21,45 @@ class IsPonshuOkay():
 class IsShuWongOkay():
     def watch():
         pass
+
+
+class StockPriceDailyPct():
+    '''
+    return any latest stock in portfolio has a daily change over threshold
+
+    Parameters
+    ----------
+    threshold: float
+        threshold to trigger alert, represent percentage 
+    '''
+
+    def __init__(self, **parm):
+        self.latest_analytic_p = db.get_most_recent_analytic_p()
+        self.threshold = parm['threshold']
+    
+    def run(self):
+        exceed = self.latest_analytic_p[self.latest_analytic_p['pct'] > self.threshold]
+        return exceed
+
+class StockPriceCumChange():
+    '''
+    return any latest stock in portfolio has a accumulative change of average price over threshold
+
+    Parameters
+    ----------
+    threshold: float
+        threshold to trigger alert, represent percentage 
+    '''
+    def __init__(self, **param):
+        self.analytic_p = db.get_portfolio_analytic_df()
+        self.portfolio_profile = db.get_most_recent_portfolio_profile()
+        self.analytic_p = self.analytic_p[self.analytic_p.time == self.analytic_p.time.max()]
+        self.threshold = param['threshold']
+
+
+    def run(self):
+        result = self.portfolio_profile.merge(self.analytic_p[['ticker', 'close']], on='ticker', suffixes=('_profile', '_analytic'))
+        result['cum_pct'] = result['close'] / result['ave_price'] - 1
+        exceed = result[result['cum_pct'] > self.threshold]
+
+        return exceed
