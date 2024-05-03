a03101cf64c87939aeaadf10675686344d5e6183
lamonkey <88therisingsun@gmail.com>
2024-05-03 18:27:02 -0400

    opportunity cost of sector's active weight

--- src/riskMonitoring/components/sectorPerformance.py
+++ src/riskMonitoring/components/sectorPerformance.py
@@ -29,6 +29,7 @@ class Component(Viewer):
         self.aw_plot = pn.pane.Plotly(self.plot_active_weight(merged_df))
         self.ar_plot = pn.pane.Plotly(self.plot_active_return(merged_df))
         self.cr_plot = pn.pane.Plotly(self.plot_cum_return(merged_df))
+        self.oppo_cost_report = pn.pane.HTML(self.get_oppo_cost_report(merged_df), sizing_mode='stretch_width')
         super().__init__(**params)
 
     @param.depends('date_range.value', watch=True)
@@ -36,6 +37,22 @@ class Component(Viewer):
         self.start_date = self.date_range.value[0]
         self.end_date = self.date_range.value[1]
 
+    def get_oppo_cost_report(self, merged_df):
+        merged_df['oppo_cost'] = merged_df.pnl * (1-merged_df['weight_b'] /
+                                                merged_df['weight_p'])
+        oppo_cost = merged_df.groupby('aggregate_sector')['oppo_cost'].sum()
+        html = '<h1> allocation机遇成本</h1>'
+        html += '<p> (1-w_b/w_p)*pnl </p>'
+        html += '<table style="width:100%; text-align:center">'
+        html += '<tr><th>Sector</th><th>Value</th></tr>'
+
+        for key, value in oppo_cost.items():
+            color = 'red' if value < 0 else 'green'
+            html += f'<tr style="color: {color};"><td>{key}</td><td>{round(value,2)}</td></tr>'
+
+        html += '</table>'
+        return html
+    
     def plot_active_weight(self, merged_df):
         fig = px.bar(merged_df, x='period',
                      y='active_weight', color='aggregate_sector',
@@ -69,6 +86,7 @@ class Component(Viewer):
         self.aw_plot.object = self.plot_active_weight(merged_df)
         self.ar_plot.object = self.plot_active_return(merged_df)
         self.cr_plot.object = self.plot_cum_return(merged_df)
+        self.oppo_cost_report.object = self.get_oppo_cost_report(merged_df)
 
     def processing(self):
         '''
@@ -118,7 +136,7 @@ class Component(Viewer):
             pn.pane.HTML(f'<h1>{self.title}</h1>'),
             self.date_range,
             pn.Tabs(
-                ('主动权重', self.aw_plot),
+                ('主动权重', pn.Column(self.oppo_cost_report, self.aw_plot)),
                 ('主动回报', self.ar_plot),
                 ('累计回报', self.cr_plot),
                 dynamic=True,
